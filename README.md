099f2fc47f69e0acb1d5d3e5409e0f9402778798
Michael McBreen <michaelmcbreen@comcast.net>
2014-10-28 15:15:27 -0700

    Added tetris project and read me

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 422_Tetris
 ==========
+This project is a testing framework for an open source version of Tetris (https://github.com/ttocs7/tetris).
+ 
---
+++ tetris-master/tetris/tetris/Program.cs
@@ -0,0 +1,23 @@
+//Program.cs
+//Written by Scott Porcaro, 2011
+using System;
+
+namespace tetris
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
